16c2ff8181519418703a1a4f0a3bfd7c30298969
tommAsoLaM <tommasolamalfa2@gmail.com>
2025-12-09 14:16:08 +0100

    changes

--- lc_nl2sql/llm_base/offline_model.py
+++ lc_nl2sql/llm_base/offline_model.py
@@ -7,7 +7,7 @@ from lc_nl2sql.configs.model_args import FinetuningArguments, GeneratingArgument
 from lc_nl2sql.llm_base.model import BaseModel
 from typing import Generator, List, Tuple, Any, Optional
 import re
-from kvpress import KnormPress, SnapKVPress, StreamingLLMPress, TOVARIPress, SimLayerPress
+from kvpress import KnormPress, SnapKVPress, StreamingLLMPress
 
 class OfflineModel(BaseModel):
     def __init__(self, model_name:str = "HuggingFaceTB/SmolLM-135M-Instruct"):
