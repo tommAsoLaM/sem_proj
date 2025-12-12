f76b14695755927b77bceb7e961629c1ef3081b9
ust 007 <ust007@tilla001.aleleon.efisonlt.com>
2025-12-12 11:20:31 +0000

    edit kvpress thing

--- lc_nl2sql/llm_base/offline_model.py
+++ lc_nl2sql/llm_base/offline_model.py
@@ -27,7 +27,7 @@ except ImportError:
 # Ensure kvpress library is installed or in path
 try:
     
-    from kvpress import KVPress, KNormPress
+    from kvpress import ExpectedAttentionPress
     KVPRESS_AVAILABLE = True
 except ImportError:
     KVPRESS_AVAILABLE = False
@@ -49,7 +49,7 @@ class OfflineModel(BaseModel):
         self.kvpress_instance = None
         self.use_kvpress = True
         # You can change default self_attn_func here
-        self.kvpress_policy = KNormPress(window_size=32, kernel_size=5) if KVPRESS_AVAILABLE else None
+        self.kvpress_policy = ExpectedAttentionPress(compression_ratio=0.4) if KVPRESS_AVAILABLE else None
         
         # Default config
         self.temperature = 0.5
