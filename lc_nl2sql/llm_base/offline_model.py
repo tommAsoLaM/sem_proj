7e66a1d3caacc17d13df8de87d76f03d41c71031
tommAsoLaM <tommasolamalfa2@gmail.com>
2025-12-12 10:05:23 +0100

    presses_changes

--- lc_nl2sql/llm_base/offline_model.py
+++ lc_nl2sql/llm_base/offline_model.py
@@ -27,7 +27,7 @@ except ImportError:
 # Ensure kvpress library is installed or in path
 try:
     
-    from kvpress import KVPress, KNormPress
+    from kvpress import KNormPress, SnapKVPress, ExpectedAttentionPress, FinchPress
     KVPRESS_AVAILABLE = True
 except ImportError:
     KVPRESS_AVAILABLE = False
@@ -48,6 +48,7 @@ class OfflineModel(BaseModel):
         # [NEW] Variables for KVPress
         self.kvpress_instance = None
         self.use_kvpress = True
+        self.presses=[]
         # You can change default self_attn_func here
         self.kvpress_policy = KNormPress(window_size=32, kernel_size=5) if KVPRESS_AVAILABLE else None
         
@@ -73,7 +74,12 @@ class OfflineModel(BaseModel):
 
         print(f"Loading local model: {self.model_name}...")
         try:
+            self.presses.append({"name":"knorm", "press": KNormPress(compression_ratio = 0.4)})
+            self.presses.append({"name":"Snap", "press": SnapKVPress(compression_ratio = 0.4)})
+            self.presses.append({"name":"Expected","press": ExpectedAttentionPress(compression_ratio = 0.4)})
+            self.presses.append({"name":"Finch", "press": FinchPress(compression_ratio = 0.4)})
             self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+            self.press = self.presses[0]["press"]
             
             # Fix for Llama models that sometimes don't have pad_token
             if self.tokenizer.pad_token_id is None:
@@ -108,6 +114,7 @@ class OfflineModel(BaseModel):
             # Do not raise fatal error, set None to be handled in chat()
             self.model = None
             self.tokenizer = None
+        
 
     def _infer_args(self, args: Optional[Dict[str, Any]] = None):
         parser = HfArgumentParser((ModelArguments, DataArguments,
