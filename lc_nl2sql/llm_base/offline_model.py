aec3069c1db1d9fccb56bf2757156c498c9bb7c9
kristonova <vista.indonesia@gmail.com>
2025-12-19 16:15:53 +0700

    edit model:Qwen/Qwen3-4B-Instruct-2507

--- lc_nl2sql/llm_base/offline_model.py
+++ lc_nl2sql/llm_base/offline_model.py
@@ -37,8 +37,8 @@ except ImportError:
 logging.basicConfig(level=logging.INFO)
 
 class OfflineModel(BaseModel):
-
-    def __init__(self, model_name:str = "meta-llama/Llama-3.2-1B-Instruct") -> None:
+# old model: meta-llama/Llama-3.2-1B-Instruct
+    def __init__(self, model_name:str = "Qwen/Qwen3-4B-Instruct-2507") -> None:
         # Initialize local model variables
         self.model_name = model_name
         self.model = None
