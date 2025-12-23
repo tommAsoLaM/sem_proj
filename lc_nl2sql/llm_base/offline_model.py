5805f8e49376ce2a3e5ddcbaebb96d5703fdf73a
mymac@Kristo-Macbook.local <rahmanto@eurecom.fr>
2025-12-23 12:22:11 +0100

    minor fix to kvpress finch parameter

--- lc_nl2sql/llm_base/offline_model.py
+++ lc_nl2sql/llm_base/offline_model.py
@@ -49,7 +49,7 @@ class OfflineModel(BaseModel):
         self.kvpress_instance = None
         self.use_kvpress = True
         # You can change default self_attn_func here
-        self.kvpress_policy = FinchPress(compression_ratio=0.4) if KVPRESS_AVAILABLE else None
+        self.kvpress_policy = FinchPress(compression_ratio=0.4, window_size=32) if KVPRESS_AVAILABLE else None
         
         # Default config
         self.temperature = 0.5
